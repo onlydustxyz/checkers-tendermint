7739a3313e025f094a0f2eef88ec96157007a40b
Stanislas Bernard <bernardstanislas@gmail.com>
2023-03-21 12:00:20 +0100

    :sparkles: Respond to queries with game board

--- app/app.go
+++ app/app.go
@@ -2,19 +2,31 @@ package app
 
 import (
 	"github.com/tendermint/tendermint/abci/types"
+	"github.com/batkinson/checkers-go/checkers"
 )
 
 type Application struct {
 	types.BaseApplication
 
+	game *checkers.Game
 	RetainBlocks int64 // blocks to retain after commit (via ResponseCommit.RetainHeight)
 }
 
 func NewApplication() *Application {
-	return &Application{}
+	return &Application{game: checkers.New()}
 }
 
 func (app *Application) Query(reqQuery types.RequestQuery) (resQuery types.ResponseQuery) {
-	resQuery.Log = "yoloooo"
+	var path = string(reqQuery.Data)
+	switch path {
+	case "/store/board":
+		resQuery.Log = "found board"
+		resQuery.Value = []byte(app.game.String())
+	case "/store/turn":
+		resQuery.Log = "found turn"
+		resQuery.Value = []byte(app.game.Turn.Color)
+	default:
+		resQuery.Log = "path not found"
+	}
 	return resQuery
 }
