ee05ebd79e1496357984a38fc70823c08fbb9b72
Stanislas Bernard <bernardstanislas@gmail.com>
2023-03-21 12:22:53 +0100

    :sparkles: Stateful app

--- app/app.go
+++ app/app.go
@@ -1,32 +1,89 @@
 package app
 
 import (
-	"github.com/tendermint/tendermint/abci/types"
+	"encoding/json"
+
 	"github.com/batkinson/checkers-go/checkers"
+	"github.com/tendermint/tendermint/abci/types"
+	dbm "github.com/tendermint/tm-db"
+)
+
+var (
+	stateKey = []byte("stateKey")
 )
 
 type Application struct {
 	types.BaseApplication
 
-	game *checkers.Game
+	db           dbm.DB
+	state        State
 	RetainBlocks int64 // blocks to retain after commit (via ResponseCommit.RetainHeight)
 }
 
+type State struct {
+	game   *checkers.Game
+	height int64
+}
+
+func loadState(db dbm.DB) State {
+	var state State
+	stateBytes, err := db.Get(stateKey)
+	if err != nil {
+		panic(err)
+	}
+	if len(stateBytes) == 0 {
+		state.game = checkers.New()
+		state.height = 0
+		return state
+	}
+	err = json.Unmarshal(stateBytes, &state)
+	if err != nil {
+		panic(err)
+	}
+	return state
+}
+
+func saveState(state State, db dbm.DB) {
+	stateBytes, err := json.Marshal(state)
+	if err != nil {
+		panic(err)
+	}
+	err = db.Set(stateKey, stateBytes)
+	if err != nil {
+		panic(err)
+	}
+}
+
 func NewApplication() *Application {
-	return &Application{game: checkers.New()}
+	db := dbm.NewMemDB()
+	state := loadState(db)
+
+	return &Application{state: state, db: db}
+}
+
+func (app *Application) Info(req types.RequestInfo) types.ResponseInfo {
+	return types.ResponseInfo{Data: string(app.state.game.String()), Version: "0.0.1", LastBlockHeight: app.state.height}
+}
+
+func (app *Application) Commit() types.ResponseCommit {
+	saveState(app.state, app.db)
+	app.state.height++
+
+	return types.ResponseCommit{}
 }
 
 func (app *Application) Query(reqQuery types.RequestQuery) (resQuery types.ResponseQuery) {
 	var path = string(reqQuery.Data)
+	resQuery.Height = app.state.height
 	switch path {
 	case "/store/board":
 		resQuery.Log = "found board"
-		resQuery.Value = []byte(app.game.String())
+		resQuery.Value = []byte(app.state.game.String())
 	case "/store/turn":
 		resQuery.Log = "found turn"
-		resQuery.Value = []byte(app.game.Turn.Color)
+		resQuery.Value = []byte(app.state.game.Turn.Color)
 	default:
 		resQuery.Log = "path not found"
 	}
-	return resQuery
+	return
 }
--- go.mod
+++ go.mod
@@ -5,17 +5,33 @@ go 1.20
 require (
 	github.com/batkinson/checkers-go v0.0.0-20150317002810-5e68444dde20
 	github.com/tendermint/tendermint v0.34.25-0.20230202213552-35581cf54ec4
+	github.com/tendermint/tm-db v0.6.6
 )
 
 require (
+	github.com/DataDog/zstd v1.4.1 // indirect
 	github.com/btcsuite/btcd v0.22.1 // indirect
+	github.com/cespare/xxhash v1.1.0 // indirect
+	github.com/dgraph-io/badger/v2 v2.2007.2 // indirect
+	github.com/dgraph-io/ristretto v0.0.3-0.20200630154024-f66de99634de // indirect
+	github.com/dgryski/go-farm v0.0.0-20190423205320-6a90982ecee2 // indirect
+	github.com/dustin/go-humanize v1.0.0 // indirect
+	github.com/facebookgo/ensure v0.0.0-20200202191622-63f1cf65ac4c // indirect
+	github.com/facebookgo/stack v0.0.0-20160209184415-751773369052 // indirect
+	github.com/facebookgo/subset v0.0.0-20200203212716-c811ad88dec4 // indirect
 	github.com/go-kit/log v0.2.1 // indirect
 	github.com/go-logfmt/logfmt v0.5.1 // indirect
 	github.com/gogo/protobuf v1.3.2 // indirect
 	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/golang/snappy v0.0.4 // indirect
+	github.com/google/btree v1.0.0 // indirect
+	github.com/jmhodges/levigo v1.0.0 // indirect
 	github.com/petermattis/goid v0.0.0-20180202154549-b0b1615b78e5 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
 	github.com/sasha-s/go-deadlock v0.3.1 // indirect
+	github.com/syndtr/goleveldb v1.0.1-0.20210819022825-2ae1ddf74ef7 // indirect
+	github.com/tecbot/gorocksdb v0.0.0-20191217155057-f0fad39f321c // indirect
+	go.etcd.io/bbolt v1.3.6 // indirect
 	golang.org/x/crypto v0.1.0 // indirect
 	golang.org/x/net v0.1.0 // indirect
 	golang.org/x/sys v0.1.0 // indirect
